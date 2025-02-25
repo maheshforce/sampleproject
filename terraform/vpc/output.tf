7d74364d33418f366c08dc6dc81587ce6c9ebb17
mahesh <konalamaheshwarareddy@gmail.com>
2025-02-25 13:59:47 +0530

    tfvpc

--- terraform/vpc/output.tf
+++ terraform/vpc/output.tf
@@ -4,9 +4,6 @@ output "vpc_details" {
     cidr_block       = aws_vpc.example.cidr_block
     public_subnets   = aws_subnet.public_subnets[*].id
     private_subnets  = aws_subnet.private_subnets[*].id
-    route_table_ids  = aws_route_table.example[*].id
-    internet_gateway = aws_internet_gateway.example.id
-    nat_gateways     = aws_nat_gateway.example[*].id
   }
   description = "Details of the created VPC and its associated resources"
 }
