9bcb25b0322651e7e34fa23a8d65c017a1041d2e
mahesh <konalamaheshwarareddy@gmail.com>
2025-02-25 14:09:16 +0530

    tfvpc

--- terraform/vpc/output.tf
+++ terraform/vpc/output.tf
@@ -1,7 +1,6 @@
 output "vpc_details" {
   value = {
     vpc_id      = module.vpc.vpc_id
-    public_subnets   = module.vpc.public_subnets
   }
   description = "Details of the created VPC and its associated resources"
 }
