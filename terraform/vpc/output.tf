863e88f8d427bc77d55a1bb1f0451248e6f255de
mahesh <konalamaheshwarareddy@gmail.com>
2025-02-25 14:02:12 +0530

    tfvpc

--- terraform/vpc/output.tf
+++ terraform/vpc/output.tf
@@ -1,7 +1,5 @@
 output "vpc_details" {
   value = {
-    vpc_id           = aws_vpc.example.id
-    cidr_block       = aws_vpc.example.cidr_block
     public_subnets   = aws_subnet.public_subnets[*].id
     private_subnets  = aws_subnet.private_subnets[*].id
   }
