f2ee79728cb39424409748b001de3021de13839d
mahesh <konalamaheshwarareddy@gmail.com>
2025-02-25 14:12:55 +0530

    tfvpc

--- terraform/vpc/output.tf
+++ terraform/vpc/output.tf
@@ -1,6 +1,12 @@
-output "vpc_details" {
+output "vpc_outputs" {
   value = {
-    vpc_id      = module.vpc.vpc_id
+    vpc_id             = module.vpc.vpc_id
+    private_subnets    = module.vpc.private_subnets
+    public_subnets     = module.vpc.public_subnets
+    nat_gateways       = module.vpc.nat_gateway_ids
+    vpn_gateway_id     = module.vpc.vpn_gateway_id
+    route_table_ids    = module.vpc.private_route_table_ids
+    public_route_table = module.vpc.public_route_table_ids
   }
-  description = "Details of the created VPC and its associated resources"
+  description = "Outputs from the VPC module"
 }
